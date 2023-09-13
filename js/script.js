d3955f3867eab9cd5b089f6c7168ed0746a2584a
Daniel_Kovalev <103295803+Dorezajure@users.noreply.github.com>
2023-09-13 18:09:13 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Работа с плашкой и кнопкой
+document.querySelector(".header-sale-img").addEventListener("click", function(){
+    document.querySelector(".header-sale").style.display = "none";
+})
