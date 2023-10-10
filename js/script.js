3db11ff9e63a891138a4eadb9b45f265c7ad6bf4
Daniel_Kovalev <103295803+Dorezajure@users.noreply.github.com>
2023-10-10 15:21:45 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@
 document.querySelector(".header-sale-img").addEventListener("click", function(){
     document.querySelector(".header-sale").style.display = "none";
 })
+
+// Блок с ценами 
+document.querySelector(".price-btn").addEventListener("click", function(){
+    document.querySelector(".price-btn").style.display = "none";
+    // Задержка создана для плавного появления блока в момент некой загрузки 
+    setTimeout(() => {
+        document.querySelector(".price-block-dop").style.display = "flex";
+    }, 200)
+})
+
+// Свайпер 
+new Swiper (".image-slider", {
+    pagination: {                       
+        el: '.swiper-pagination',
+        clickable: true,     
+    },
+});
+
+new Swiper (".reviews-slider", {
+    pagination: {                       
+        el: '.swiper-pagination',
+        clickable: true,     
+    },
+});
